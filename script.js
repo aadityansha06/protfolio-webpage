2628d6c317e806aac4cd7f723b3a36d4a6ab3ae1
aadityansha06 <pkumariverma42@gmail.com>
2025-01-24 19:17:27 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    const mainContent = document.getElementById('main-content');
+  
+    preloader.style.display = 'none';
+    mainContent.style.display = 'block';
+  });
