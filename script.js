ff91c7e5ecfd5f5da1cf8b7354194ff376b0e0fa
aadityansha06 <pkumariverma42@gmail.com>
2025-01-24 20:26:10 +0530

    newly updated

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 window.addEventListener('load', () => {
     const preloader = document.getElementById('preloader');
     const mainContent = document.getElementById('main-content');
   
     preloader.style.display = 'none';
     mainContent.style.display = 'block';
-=======
-window.addEventListener('load', () => {
-    const preloader = document.getElementById('preloader');
-    const mainContent = document.getElementById('main-content');
-  
-    preloader.style.display = 'none';
-    mainContent.style.display = 'block';
->>>>>>> 378cc2c70801f24936e8bd382cacef6cac5c2c97
   });
